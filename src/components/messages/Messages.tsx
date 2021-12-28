b395cb4145da918a037a4ab37e590ddecd620cff
Talman95 <taltro528@gmail.com>
2021-12-28 22:55:08 +0300

    added styles for the messages

--- src/components/messages/Messages.tsx
+++ src/components/messages/Messages.tsx
@@ -1,9 +1,38 @@
 import React from 'react';
+import classes from './Messages.module.css';
+import userLogo from './../../assets/images/userLogo.png';
 
 const Messages = () => {
     return (
-        <div>
-            Сообщения!
+        <div className={classes.chatbox}>
+            <div className={classes.header}>
+                <img src={userLogo} alt='user' />
+                <div className={classes.person}>
+                    <span className={classes.name}>Anna Luzhina</span>
+                    <span className={classes.status}>Online</span>
+                </div>
+                <a href="">
+                    <img src="" alt=""/>
+                </a>
+            </div>
+
+            <div className={classes.chat_logs}>
+                <div className={classes.chat}>
+                    <div className={classes.user_photo}>
+                        <img src={userLogo} alt=""/>
+                    </div>
+                    <p className={classes.chat_message}>
+                        something else...
+                    </p>
+                </div>
+            </div>
+
+            <div className={classes.chatForm}>
+                <div className={classes.textarea}>
+                    <textarea rows={3} placeholder={"Enter your message"}></textarea>
+                </div>
+                <button>Send</button>
+            </div>
         </div>
     );
 };
