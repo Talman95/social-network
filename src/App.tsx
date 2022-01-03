d91cf4828c2019d11bb32b3e4e6c45c6b9af7d25
Talman95 <taltro528@gmail.com>
2022-01-03 22:16:55 +0300

    added the map method in the Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,16 @@ import Messages from "./components/messages/Messages";
 import './App.css';
 
 function App() {
+    let chats = [
+        {name: 'Dmitrii Antonov', lastMessage: 'Hey. Do you have any props?', notice: 1, date: '12:33'},
+        {name: 'Artyom Vasiliev', lastMessage: 'Let\'s get it started!', notice: 11, date: '00:19'},
+        {name: 'Dariya Bugaeva', lastMessage: 'Merry Christmas', notice: 2, date: '23:07'}
+    ]
   return (
     <div className="app">
         <Header />
         <div className='app_container'>
-            <Sidebar />
+            <Sidebar chats={chats} />
             <Messages name={'Someone\'s Name'} status={'Online'}/>
         </div>
     </div>
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -3,8 +3,17 @@ import white_search from './../../assets/images/white_search.png';
 import classes from './Sidebar.module.css';
 import SidebarItem from './sidebarItem/SidebarItem';
 
+type propsType = {
+    chats: Array<inArray>
+}
+type inArray = {
+    name: string
+    lastMessage: string
+    notice: number
+    date: string
+}
 
-const Sidebar = (props: any) => {
+const Sidebar = (props: propsType) => {
     return (
         <div className={classes.sidebar}>
             <div className={classes.search_chat}>
@@ -16,9 +25,11 @@ const Sidebar = (props: any) => {
                 </div>
             </div>
             <div className={classes.chats}>
-                <SidebarItem name={'Dmitrii Antonov'} lastMessage={'Hey. Do you have any props?'} notice={1} date={'12:33'}/>
-                <SidebarItem name={'Artyom Vasiliev'} lastMessage={'Let\'s get it started!'} notice={11} date={'00:19'}/>
-                <SidebarItem name={'Dariya Bugaeva'} lastMessage={'Merry Christmas'} notice={2} date={'23:07'}/>
+                {props.chats.map(m => {
+                    return (
+                        <SidebarItem name={m.name} lastMessage={m.lastMessage} notice={m.notice} date={m.date} />
+                    )
+                })}
             </div>
         </div>
     );
