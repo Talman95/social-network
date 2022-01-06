459e537e64475ec0cd8e62dd3a6839022bab0a00
Talman95 <taltro528@gmail.com>
2022-01-06 14:16:50 +0300

    changed the layout on the sidebar page. added user info and changed the input search

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import white_search from './../../assets/images/white_search.png';
 import classes from './Sidebar.module.css';
 import SidebarItem from './sidebarItem/SidebarItem';
+import userLogo from '../../assets/images/userLogo.png';
+import search from "../../assets/images/search.png";
 
 type PropsType = {
     chats: Array<inArray>
@@ -16,18 +18,22 @@ type inArray = {
 const Sidebar: React.FC<PropsType> = (props) => {
     return (
         <div className={classes.sidebar}>
-            <div className={classes.search_chat}>
-                <div className={classes.search_box}>
-                    <div className={classes.search_img}>
-                        <img src={white_search} alt={'search'}/>
-                    </div>
-                    <input type='text' placeholder='Search to start new chat'/>
+            <div className={classes.profile_info}>
+                <img className={classes.profile_img} src={userLogo} alt={'Profile'}/>
+                <span>
+
+                </span>
+            </div>
+            <div className={classes.search_box}>
+                <div className={classes.search_button}>
+                    <img src={search} alt="search button"/>
                 </div>
+                <input type="text" placeholder="Search here..." />
             </div>
             <div className={classes.chats}>
                 {props.chats.map(m => {
                     return (
-                        <SidebarItem name={m.name} lastMessage={m.lastMessage} notice={m.notice} date={m.time} />
+                        <SidebarItem name={m.name} lastMessage={m.lastMessage} notice={m.notice} date={m.time}/>
                     )
                 })}
             </div>
