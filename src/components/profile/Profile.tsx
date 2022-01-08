a0d77eff819fb2340a415654e727e339bcb40964
Talman95 <taltro528@gmail.com>
2022-01-08 23:12:32 +0300

    started to edit the profile page

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import classes from './Profile.module.css';
+import user from '../../assets/images/userLogo.png';
 
 export const Profile: React.FC<any> = (props) => {
     return (
         <div className={classes.profile}>
-            <div className={classes.content}>
-                <p>Profile</p>
+            <div className={classes.header}>
+                <div className={classes.photo}>
+                    <img src={user} />
+                    <button>Редактировать</button>
+                </div>
+                <div className={classes.info}>
+                    <div className={classes.about}>
+                        <span className={classes.name}>Roman Talman</span>
+                        <span className={classes.online}>Online</span>
+                    </div>
+                    <span className={classes.status}>Set the status</span>
+                </div>
             </div>
         </div>
     );
