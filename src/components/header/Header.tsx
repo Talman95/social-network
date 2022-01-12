2977983fef5b251e15f42b8e92a293ae2436fb07
Talman95 <taltro528@gmail.com>
2022-01-12 19:15:23 +0300

    changed HTML coding of the Header component

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
-import classes from './Header.module.css';
+import cl from './Header.module.css';
 import menu from './../../assets/images/menu.png';
-import search from './../../assets/images/search.png';
-import right_menu from './../../assets/images/right-menu.png';
+import user from '../../assets/images/userLogo.png'
+import {MyInput} from "../UI/input/MyInput";
 
 const Header = () => {
     return (
-        <div className={classes.header}>
-            <div className={classes.header_left}>
-                <img src={menu} alt="menu" />
+        <div className={cl.header}>
+            <div className={cl.header_user_icon}>
+                <a href={'/profile'}>
+                    <img src={user} alt="User"/>
+                    <p>Talman Roman</p>
+                </a>
             </div>
-                <div className={classes.header_right}>
-                    <div className={classes.search_box}>
-                        <a className={classes.search_button}>
-                            <img src={search} alt="search button"/>
-                        </a>
-                        <input type="text" placeholder="Search ..." />
-                    </div>
-                    <div className={classes.right_menu}>
-                        <img src={right_menu} alt='right menu' />
-                    </div>
+
+            <div className={cl.header_right}>
+                <div className={cl.input}>
+                    <MyInput/>
                 </div>
+                <div className={cl.right_menu}>
+                    <img src={menu} alt='right menu'/>
+                </div>
+            </div>
         </div>
     );
 };
