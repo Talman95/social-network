744f1cb4f97343a52b251b4bc96f074366602947
Talman95 <taltro528@gmail.com>
2022-01-13 21:13:33 +0300

    updated the sidebar component

--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import classes from './Sidebar.module.css';
+import {NavLink} from "react-router-dom";
+import user from '../../assets/images/userLogo.png';
 
-export const Sidebar = () => {
+
+export const Sidebar: React.FC<any> = (props) => {
     return (
-        <div>
+        <div className={classes.sidebar}>
+            <div className={classes.sidebar_tittle}>
+                <h4>Following</h4>
+                <NavLink to="/">
+                    Hide Chat
+                </NavLink>
+            </div>
 
+            <div className={classes.online_list}>
+                <div className={classes.online}>
+                    <img src={user} alt="User logo" />
+                </div>
+                <p>Full Name</p>
+            </div>
         </div>
     );
 };
