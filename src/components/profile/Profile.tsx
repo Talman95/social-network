d9675246e9b11b99e5056c08242a759c7b03921c
Talman95 <taltro528@gmail.com>
2022-01-16 23:16:27 +0300

    changed the profile page

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -6,26 +6,13 @@ import {MyButton} from "../UI/button/MyButton";
 export const Profile: React.FC<any> = (props) => {
     return (
         <div className={cl.profile}>
-            <div className={cl.header}>
-                <div className={cl.photo}>
-                    <img src={user} alt={'User'}/>
-                    <MyButton>Edit</MyButton>
-                    <div className={cl.job}>
-                        <input type={'checkbox'} checked={true}/> <span>Ищу работу</span>
-                    </div>
-                </div>
-                <div className={cl.info}>
-                    <div className={cl.about}>
-                        <span className={cl.name}>Roman Talman</span>
-                        <span className={cl.online}>Online</span>
-                    </div>
-                    <span className={cl.status}>Set the status</span>
-                </div>
-            </div>
-
             <div className={cl.write_post_container}>
                 <div className={cl.user_profile}>
-                    <img src={user} alt=""/>
+                    <img src={user} alt="user"/>
+                    <div>
+                        <p>Roman Talmanff</p>
+                        <span className={cl.status}>Set the status</span>
+                    </div>
                 </div>
                 <div className={cl.post_input_container}>
                     <textarea rows={3} placeholder={"How you doin?"}></textarea>
