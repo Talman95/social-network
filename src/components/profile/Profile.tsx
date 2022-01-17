d490d836ab788f3686c69d4c40c9074a06dceae6
Talman95 <taltro528@gmail.com>
2022-01-17 22:44:06 +0300

    update profile. added post

--- src/components/profile/Profile.tsx
+++ src/components/profile/Profile.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import cl from './Profile.module.css';
 import user from '../../assets/images/userLogo.png';
 import {MyButton} from "../UI/button/MyButton";
+import like from '../../assets/images/like.png';
+import comments from '../../assets/images/like.png';
 
 export const Profile: React.FC<any> = (props) => {
     return (
@@ -18,6 +20,24 @@ export const Profile: React.FC<any> = (props) => {
                     <textarea rows={3} placeholder={"How you doin?"}></textarea>
                     <div className={cl.add_post_button}>
                         <MyButton>Add a post</MyButton>
+                    </div>
+                </div>
+            </div>
+            <div className={cl.post_container}>
+                <div className={cl.user_profile}>
+                    <img src={user} alt="user" />
+                    <div>
+                        <p>Roman Talman</p>
+                        <span>time</span>
+                    </div>
+                </div>
+
+                <p className={cl.post_text}>Something text</p>
+
+                <div className={cl.post_row}>
+                    <div className={cl.activity_icons}>
+                        <div><img src={like} alt="like" />5</div>
+                        <div><img src={comments} alt="comments" />2</div>
                     </div>
                 </div>
             </div>
