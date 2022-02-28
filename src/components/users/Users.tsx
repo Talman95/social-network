1e87ba7d6355c3e123eb00a18edb197f82572a69
Talman95 <taltro528@gmail.com>
2022-02-28 17:24:37 +0300

    started using class component for Users

--- src/components/users/Users.tsx
+++ src/components/users/Users.tsx
@@ -1,28 +1,31 @@
-import React, {FC} from 'react';
+import React from 'react';
 import cl from './Users.module.css';
 import {User} from "./User/User";
 import {UsersContainerPropsType} from "./UsersContainer";
 import axios from "axios";
 
-export const Users: FC<UsersContainerPropsType> = (props) => {
 
-    if (props.users.length === 0) {
+export class Users extends React.Component<UsersContainerPropsType> {
+    constructor(props: UsersContainerPropsType) {
+        super(props)
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
-            props.setUsers(response.data.items)
+            this.props.setUsers(response.data.items)
         })
     }
+    
+    render() {
+        const mappedUsers = this.props.users.map((u, index) => <User
+                key={index}
+                user={u}
+                follow={this.props.follow}
+                unfollow={this.props.unfollow}
+            />
+        )
 
-    const mappedUsers = props.users.map((u, index) => <User
-            key={index}
-            user={u}
-            follow={props.follow}
-            unfollow={props.unfollow}
-        />
-    )
-
-    return (
-        <div className={cl.container}>
-            {mappedUsers}
-        </div>
-    );
-};
+        return (
+            <div className={cl.container}>
+                {mappedUsers}
+            </div>
+        );
+    }
+}
